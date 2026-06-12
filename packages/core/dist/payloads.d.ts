23845fb586b67ae7d945a5d7215e7e5b6feb01de
SecH0us3 <github@ioc.slmail.me>
2026-06-13 00:05:16 +0300

    chore(gitignore): remove accidentally committed dist build artifacts from Git tracking

--- packages/core/dist/payloads.d.ts
+++
@@ -1,9 +0,0 @@
-export type PayloadCategory = {
-    type: 'ParamCheck' | 'FileCheck' | 'Header';
-    payloads: string[];
-    falsePayloads: string[];
-};
-export declare const PAYLOADS: Record<string, PayloadCategory>;
-export declare const ENHANCED_PAYLOADS: {
-    [x: string]: PayloadCategory;
-};
--- packages/core/dist/utils/security.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function isValidTargetUrl(urlString: string): boolean;
